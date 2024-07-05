6efb0eaa9450e9c33f141b3074fc6e5c142f496b
Madhurima Saha <it.20213057@gmail.com>
2024-07-05 13:12:22 +0530

    commit

---
+++ pattern_1.py
@@ -0,0 +1,16 @@
+def print_pyramid(n):
+
+    for i in range(n):
+      
+        for j in range(n - i - 1):
+            print(" ", end="")
+        
+        
+        for k in range(2 * i + 1):
+            print("*", end="")
+        
+       
+        print()
+
+
+print_pyramid(5)
