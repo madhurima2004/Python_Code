dc969ce45736133dba1abb487b756600d771d26f
Madhurima Saha <it.20213057@gmail.com>
2024-07-06 11:39:33 +0530

    commit

---
+++ pattern_4.py
@@ -0,0 +1,11 @@
+def number_pyramid(n):
+    for i in range(1, n+1):
+        for j in range(n-i):
+            print(" ", end="")
+        for j in range(1, i+1):
+            print(j, end="")
+        for j in range(i-1, 0, -1):
+            print(j, end="")
+        print()
+
+number_pyramid(5)
