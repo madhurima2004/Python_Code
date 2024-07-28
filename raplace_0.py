4b2aa65f9cf6d6ed79a7745a4ff98a07a98f16e0
Madhurima Saha <it.20213057@gmail.com>
2024-07-28 20:39:48 +0530

    commit

--- raplace_0.py
+++ raplace_0.py
@@ -1,6 +1,6 @@
 def replace_zeros_in_list(numbers):
     return [1 if x == 0 else x for x in numbers]
 
-numbers = [0, 4, 0, 5, 2, 0, 9, 0]
+numbers = [0, 4, 0, 0, 2, 0, 9, 0]
 new_numbers = replace_zeros_in_list(numbers)
 print(new_numbers)
